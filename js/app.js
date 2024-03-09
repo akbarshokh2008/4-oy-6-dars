1201f712b33b355127311de626f82f4f856cd270
akbarshokh2008 <mosinovakbarjon9@gmail.com>
2024-03-09 20:51:32 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,77 @@
+// 47- MASALA
+
+// let xabarlar = +prompt("ovozlarni kiriting");
+// let inkorlar = +prompt("inkorlarni kiriting");
+// function ovozlar(plus, minus) {
+//   let result = `jami ovozlar ${plus - minus}`;
+//   return result;
+// }
+// console.log(ovozlar(xabarlar, inkorlar));
+
+// 48- MASALA
+// let raqam = +prompt("son kiriting");
+// function negativ(son) {
+//   let result = son - (son + son);
+//   return result;
+// }
+// console.log(negativ(raqam));
+
+// 49 - MASALA
+// const arr = [1, 2, 3, 4, 5];
+// function almash(x) {
+//   let result = x.reverse();
+//   return result;
+// }
+// console.log(almash(arr));
+
+// 51 - MASALA
+// let masiv = [1, 2, 3, 4, 5];
+// function qoshishMassivga(masiv) {
+//   for (let i = 0; i < masiv.length; i++) {
+//     masiv[i] += 1;
+//   }
+//   return masiv;
+// }
+// console.log(qoshishMassivga(masiv));
+
+// 52 - MASALA
+// let ism = "Donyor";
+// let familya = "Olimov";
+// let natija = `Natija = ${ism} ${familya}`;
+// console.log(natija);
+
+// 53 - MASALA
+
+// let holati = prompt("Holatni kiriting:");
+// holati = holati === "yaxshi" ? "yaxshi" : "yaxshi emas";
+
+// 56 - MASALA
+// let arr = [1, "gilos", 3, 4, true];
+// function songiElement(arr) {
+//   let result = arr[arr.length - 1];
+//   return result;
+// }
+// console.log(songiElement(arr));
+
+// 62 - MASALA
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// function arrayToString(arr) {
+//   return arr.join("");
+// }
+// console.log(arrayToString(arr));
+
+// 63 - MASALA
+
+// let arr = [1, 2, 3, 4], [(5, 6, 7)];
+// console.log(arr);
+
+// 64 - MASALA
+arr = [olma, anor, gilos, olcha];
+let top = prompt("shularda bittasini yozin :  olma, anor, gilos, olcha");
+function topIndex(arr, str) {
+  let result = arr[arr.indexOF(anor)];
+  return result;
+}
+
+console.log(topIndex(arr));
